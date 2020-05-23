9b0ed76b14e48076ef24266b189b05dad3fdb701
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2020-05-23 14:45:46 -0600

    ...

---
+++ Assets/flashlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlight : MonoBehaviour
+{
+    public Light lantern;
+    public bool on = true;
+    void Update()
+    {
+       if(Input.GetKey("Mouse1"))
+        {
+            if (on == true)
+            {
+                lantern.enabled = true;
+                on = false;
+            }
+            else
+            {
+                lantern.enabled = true;
+                on = true;
+            }
+        }
+    }
+}
